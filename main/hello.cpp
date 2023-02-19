24fb49fa65f56f50aa6101b99a73c30a255a40ad
Pratham Kulkarni <79354193+prathamkulkarni@users.noreply.github.com>
2023-02-19 20:25:40 +0530

    Add files via upload

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I am Pratham, PES1UG20CS307\n";
+  cout << "I am Pratham Kulkarni, PES1UG20CS307\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
